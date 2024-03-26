bc6c675d87584b4d0543f7d6663073db88876248
blackchapel <kc.dark@protonmail.com>
2024-03-26 16:44:18 +0530

    added new url

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -3,3 +3,4 @@ export const LOGIN_PATH = 'auth/login';
 export const REGISTER_PATH = 'auth/signup';
 export const OTP_PATH = 'auth/verify-otp';
 export const GET_EVENTS = 'guardian/events';
+export const POST_MEDICINE = 'medicine';
